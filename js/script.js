510863d72181ad0c6c9a55d99083dfd15039ec12
yewon_s <seoyewon93@gmail.com>
2023-04-25 16:27:37 -0400

    Changed style and text of button

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ var cat = document.querySelector(".cat");
 // console.log(cat);
 
 button.addEventListener("click", function () {
-    // console.log("It's working!");
-    cat.classList.add("show");
+    if (cat.classList.contains("show")) {
+        // console.log("Yes");
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
+    } else {
+        // console.log("No");
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!";
+        button.classList.remove("disappear");
+    }
 });
